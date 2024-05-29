d5ff23609891d005acc0e97faf7df142edfe35d5
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-05-29 13:12:25 +0530

    update: **hide list view of cards in small size ** conditionally triggering diffrent card ui

--- src/components/ele/Products.Cards.tsx
+++ src/components/ele/Products.Cards.tsx
@@ -10,25 +10,46 @@ export function Cards(props: ICardProp) {
 
     const renderView = () => {
         {   
-            if(tableView === "grid") {
+            if(tableView === "grid" || props.cardSize === 'small') {
                 return <GridView cardSize={props.cardSize}/>
             } else {
                 return <ListView />
             }
         }
     }
+
     return (
-        <div className="container mt-2 d-flex flex-column mb-lg-0 product_container">
-            <div className="product_view d-flex flex-row justify-content-end">
+        <div className="container mt-2 d-flex flex-column mb-lg-0 product_container px-0">
+            <div className={props.cardSize !== 'small' ? 'product_view d-flex flex-row justify-content-end' : 'd-none'}>
                 <div className="btn-group">
                     <a className={`btn btn-outline-secondary border-2 ${tableView === "list" ? "active" : '' }`} onClick={() => setTableView("list")} aria-current="page"><i className="bi bi-list-ul"></i></a>
                     <a className={`btn btn-outline-secondary border-2 ${tableView === "grid" ? "active" : '' }`} onClick={() => setTableView("grid")}><i className="bi bi-grid-3x3"></i></a>
                 </div>
             </div>
-            <div className="conainer d-flex flex-column">
-                <div className="row p-2 justify-content-center gap-2">
-                    {renderView()}
-                </div>
+            <div className="conainer d-flex flex-column mb-2">
+                {
+                    props.cardSize !== 'small' ?
+                        <div className="row p-2 justify-content-center gap-2">
+                            {renderView()}
+                        </div>
+                        :
+                        <div className="p-2 d-flex gap-2 similar-prodcts-container">
+                            {renderView()}
+                            {renderView()}
+                            {renderView()}
+                            {renderView()}
+                            {renderView()}
+                            {renderView()}
+                            {renderView()}
+                            {renderView()}
+                            {renderView()}
+                            {renderView()}
+                            {renderView()}
+                            {renderView()}
+                            {renderView()}
+                        </div>
+
+                }
             </div>
 
         </div>
