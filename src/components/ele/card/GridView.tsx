60e8315eca2a5a8c7e9bc33463d063c5fb5342a1
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-05-29 13:08:17 +0530

    update:
    * hide buttons on small size
    ** updated ui

--- src/components/ele/card/GridView.tsx
+++ src/components/ele/card/GridView.tsx
@@ -12,16 +12,24 @@ export default function GridView (props : ICardProp) {
             }))
         } else if(props.cardSize === 'small') {
             setCardProp(() => ({
-                width : '9rem'
+                width : '10rem'
             }))
         }
-    })
+    },[props.cardSize])
+    
     return (
         <div className="card p-0 col-12 col-sm-6 col-md-4 col-lg-3" style={cardProp}>
             <img src="https://i.imgur.com/QkIa5tT.jpeg" className="card-img-top" alt="..." />
-            <div className="card-body px-2 py-2">
-                <p className="card-title card_text">Classic White Tee - Timeless Style and Comfort</p>
-                <div className="d-flex flex-row justify-content-between">
+            <div className={props.cardSize !== "small" ? "card-body px-2 py-2" : "card-body p-1"}>
+                {
+                    props.cardSize !== "small" ? 
+                        <p className="card-title card_text">Classic White Tee - Timeless Style and Comfort</p>
+                        :
+                        <a href="" className='text-decoration-none'>
+                            <p className="card-title card_text_small m-0" >Classic White Tee - Timeless Style and Comfort</p>
+                        </a>
+                }
+                <div className={`d-flex flex-row justify-content-between ${props.cardSize === 'small' && 'd-none'}`}>
                     <button type="button" className="btn btn-success product_button">Buy Now <i className="bi bi-bag product_button"></i></button>
                     <button type="button" className="btn btn-warning product_button">Add to Cart <i className="bi bi-cart-plus product_button"></i></button>
                 </div>
