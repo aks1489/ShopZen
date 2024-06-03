10d4b5192ec77d5a424325182bc4edf9096ace18
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-06-03 15:05:36 +0530

    added price in list view

--- src/components/ele/card/ListView.tsx
+++ src/components/ele/card/ListView.tsx
@@ -12,6 +12,11 @@ export default function ListView () {
                         <p className='p-0 m-0 lh-sm product_text mb-md-1'>Classic White Tee - Timeless Style and Comfort"</p>
                         <p className='product_description p-0 m-0 d-none mh-md-50 overflow-auto d-sm-block'>Elevate your wardrobe with this stylish black t-shirt featuring a striking monochrome mountain range graphic. Perfect for those who love the outdoors or want to add a touch of nature-inspired design to their look, this tee is crafted from soft, breathable fabric ensuring all-day comfort. Ideal for casual outings or as a unique gift, this t-shirt is a versatile addition to any collection.</p>
                     </div>
+                    <div className="d-flex align-items-center gap-2">
+                        <h2 className="fw-bolder m-0"><i className="bi bi-currency-dollar"></i>300</h2>
+                        <div className="price_text_discount text-decoration-line-through fw-normal text-secondary ">$2000</div>
+                        <div className="price_text text-success fw-medium">18 % off</div>
+                    </div>
                     <div className="product_button d-flex flex-row gap-2 mb-1 justify-content-evenly justify-content-sm-start px-2">
                         <button type="button" className="btn btn-success product_button">{"Buy Now"} <i className="bi bi-bag product_button"></i></button>
                         <button type="button" className="btn btn-warning product_button">{"Add to Cart"} <i className="bi bi-cart-plus product_button"></i></button>
