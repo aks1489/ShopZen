0434841175a9c6109a4c20918e6d40113422544a
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-06-08 12:26:00 +0530

    modify:
    *list view price section and remove cart button marged
    * conditionally rendering list price section
    * price section moved to listData() function

--- src/components/ele/card/ListView.tsx
+++ src/components/ele/card/ListView.tsx
@@ -10,6 +10,31 @@ export default function ListView (props : IListView) {
         setDiscountAmount((props.cartData.price * props.cartData.discountPercentage) / 100);
         setPrice(props.cartData.price + discountAmount);
     })
+
+    const listData = () =>{
+        if(props.data === 'cart') {
+            return (
+                <div className="d-flex justify-content-between">
+                    <div className="d-flex align-items-center gap-2">
+                        <h2 className="fw-bolder m-0"><i className="bi bi-currency-dollar"></i>{props.cartData.price}</h2>
+                        <div className="price_text_discount text-decoration-line-through fw-normal text-secondary text-decoration-line-through"><i className="bi bi-currency-dollar"></i>{price.toFixed(2)}</div>
+                        <div className="price_text text-success fw-medium">{props.cartData.discountPercentage} % off</div>
+                    </div>
+                    <div className="product_button d-flex flex-row gap-2 mb-1 justify-content-evenly justify-content-sm-start px-2">
+                        <button type="button" className="btn btn-danger product_button">{"Remove from Cart"} <i className="bi bi-trash3-fill"></i></button>
+                    </div>
+                </div>
+            )
+        } else if (props.data === 'show') {
+            return (
+                <div className="d-flex align-items-center gap-2">
+                    <h2 className="fw-bolder m-0"><i className="bi bi-currency-dollar"></i>{props.cartData.price}</h2>
+                    <div className="price_text_discount text-decoration-line-through fw-normal text-secondary text-decoration-line-through"><i className="bi bi-currency-dollar"></i>{price.toFixed(2)}</div>
+                    <div className="price_text text-success fw-medium">{props.cartData.discountPercentage} % off</div>
+                </div>
+            )
+        }
+    }
     console.log(props)
     return (
         <div className="card px-0 col-12">
@@ -22,16 +47,13 @@ export default function ListView (props : IListView) {
                         <p className='p-0 m-0 lh-sm product_text mb-md-1'>{props.cartData.title}</p>
                         <p className='product_description p-0 m-0 d-none mh-md-50 overflow-auto d-sm-block'>{props.cartData.description}</p>
                     </div>
-                    <div className="d-flex align-items-center gap-2">
-                        <h2 className="fw-bolder m-0"><i className="bi bi-currency-dollar"></i>{props.cartData.price}</h2>
-                        <div className="price_text_discount text-decoration-line-through fw-normal text-secondary text-decoration-line-through"><i className="bi bi-currency-dollar"></i>{price.toFixed(2)}</div>
-                        <div className="price_text text-success fw-medium">{props.cartData.discountPercentage} % off</div>
-                    </div>
-                    { props.data === 'show' && 
-                        <div className="product_button d-flex flex-row gap-2 mb-1 justify-content-evenly justify-content-sm-start px-2">
-                            <button type="button" className="btn btn-success product_button">{"Buy Now"} <i className="bi bi-bag product_button"></i></button>
-                            <button type="button" className="btn btn-warning product_button">{"Add to Cart"} <i className="bi bi-cart-plus product_button"></i></button>
-                        </div>
+                   {listData()}
+                    {
+                        props.data === 'show' && 
+                            <div className="product_button d-flex flex-row gap-2 mb-1 justify-content-evenly justify-content-sm-start px-2">
+                                <button type="button" className="btn btn-success product_button">{"Buy Now"} <i className="bi bi-bag product_button"></i></button>
+                                <button type="button" className="btn btn-warning product_button">{"Add to Cart"} <i className="bi bi-cart-plus product_button"></i></button>
+                            </div>
                     }
                 </div>
             </div>
