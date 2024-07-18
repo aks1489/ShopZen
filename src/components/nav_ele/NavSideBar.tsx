5d58646382db6d42cfc1af8da5da0aef4eeed359
akash.sarkar1489 <akash.sarkar1489@gmail.com>
2024-07-18 10:50:12 +0000

    updated side nav

--- src/components/nav_ele/NavSideBar.tsx
+++ src/components/nav_ele/NavSideBar.tsx
@@ -2,24 +2,21 @@ export default function NavSideBar() {
     return (
         <div className="offcanvas offcanvas-start navbar-toggler" tabIndex={-1} id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
             <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
+                <h5 className="offcanvas-title" id="offcanvasExampleLabel"><img src="/shopzen.png" className='nav_logo' alt="shopzen" /></h5>
                 <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div className="offcanvas-body">
-                <div className="d-flex gap-2">
+                <p className="p-0 m-0">Navigation</p>
+                <div className="d-flex gap-2 my-2">
                     <button type="button" className="btn btn-outline-secondary">Home</button>
                     <button type="button" className="btn btn-outline-secondary">Profile</button>
                 </div>
-                <div className="dropdown mt-3">
+                <form className="d-flex my-2" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Write to search...." aria-label="Search" />
+                    <button className="btn btn-outline-success" type="submit"><i className="bi bi-search"></i></button>
+                </form>
+                <hr className="p-0 my-2"/>
                 <p className="p-0 m-0">Categories</p>
-                <hr className="p-0 mt-2"/>
-                
-                <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-                </div>
             </div>
         </div>
     )
