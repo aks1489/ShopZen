8f4da351820c34fff014da1f982cce59a86e054c
akash.sarkar1489 <akash.sarkar1489@gmail.com>
2024-07-19 06:00:15 +0000

    update hard data

--- src/components/pages/data.ts
+++ src/components/pages/data.ts
@@ -1,62 +1,62 @@
 export const data = {
-  "id": 8,
-  "title": "Dior J'adore",
-  "description": "J'adore by Dior is a luxurious and floral fragrance, known for its blend of ylang-ylang, rose, and jasmine. It embodies femininity and sophistication.",
+  "id": 10,
+  "title": "Gucci Bloom Eau de",
+  "description": "Gucci Bloom by Gucci is a floral and captivating fragrance, with notes of tuberose, jasmine, and Rangoon creeper. It's a modern and romantic scent.",
   "category": "fragrances",
-  "price": 89.99,
-  "discountPercentage": 17.44,
-  "rating": 3.31,
-  "stock": 91,
+  "price": 79.99,
+  "discountPercentage": 8.9,
+  "rating": 2.69,
+  "stock": 93,
   "tags": [
     "fragrances",
     "perfumes"
   ],
-  "brand": "Dior",
-  "sku": "E70NB03B",
+  "brand": "Gucci",
+  "sku": "FFKZ6HOF",
   "weight": 10,
   "dimensions": {
-    "width": 21.51,
-    "height": 7,
-    "depth": 26.51
+    "width": 22.28,
+    "height": 17.81,
+    "depth": 27.18
   },
-  "warrantyInformation": "Lifetime warranty",
+  "warrantyInformation": "No warranty",
   "shippingInformation": "Ships in 2 weeks",
   "availabilityStatus": "In Stock",
   "reviews": [
     {
       "rating": 5,
-      "comment": "Fast shipping!",
-      "date": "2024-05-23T08:56:21.619Z",
-      "reviewerName": "Zoe Nicholson",
-      "reviewerEmail": "zoe.nicholson@x.dummyjson.com"
+      "comment": "Great value for money!",
+      "date": "2024-05-23T08:56:21.620Z",
+      "reviewerName": "Aria Parker",
+      "reviewerEmail": "aria.parker@x.dummyjson.com"
     },
     {
       "rating": 4,
       "comment": "Excellent quality!",
-      "date": "2024-05-23T08:56:21.619Z",
-      "reviewerName": "Addison Wright",
-      "reviewerEmail": "addison.wright@x.dummyjson.com"
+      "date": "2024-05-23T08:56:21.620Z",
+      "reviewerName": "Natalie Harris",
+      "reviewerEmail": "natalie.harris@x.dummyjson.com"
     },
     {
       "rating": 4,
-      "comment": "Would buy again!",
-      "date": "2024-05-23T08:56:21.619Z",
-      "reviewerName": "Clara Berry",
-      "reviewerEmail": "clara.berry@x.dummyjson.com"
+      "comment": "Fast shipping!",
+      "date": "2024-05-23T08:56:21.620Z",
+      "reviewerName": "Ava Harris",
+      "reviewerEmail": "ava.harris@x.dummyjson.com"
     }
   ],
-  "returnPolicy": "7 days return policy",
-  "minimumOrderQuantity": 8,
+  "returnPolicy": "No return policy",
+  "minimumOrderQuantity": 10,
   "meta": {
-    "createdAt": "2024-05-23T08:56:21.619Z",
-    "updatedAt": "2024-05-23T08:56:21.619Z",
-    "barcode": "0887083199279",
+    "createdAt": "2024-05-23T08:56:21.620Z",
+    "updatedAt": "2024-05-23T08:56:21.620Z",
+    "barcode": "8232190382069",
     "qrCode": "https://assets.dummyjson.com/public/qr-code.png"
   },
   "images": [
-    "https://cdn.dummyjson.com/products/images/fragrances/Dior%20J'adore/1.png",
-    "https://cdn.dummyjson.com/products/images/fragrances/Dior%20J'adore/2.png",
-    "https://cdn.dummyjson.com/products/images/fragrances/Dior%20J'adore/3.png"
+    "https://cdn.dummyjson.com/products/images/fragrances/Gucci%20Bloom%20Eau%20de/1.png",
+    "https://cdn.dummyjson.com/products/images/fragrances/Gucci%20Bloom%20Eau%20de/2.png",
+    "https://cdn.dummyjson.com/products/images/fragrances/Gucci%20Bloom%20Eau%20de/3.png"
   ],
-  "thumbnail": "https://cdn.dummyjson.com/products/images/fragrances/Dior%20J'adore/thumbnail.png"
+  "thumbnail": "https://cdn.dummyjson.com/products/images/fragrances/Gucci%20Bloom%20Eau%20de/thumbnail.png"
 }
